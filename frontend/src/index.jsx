ecbc97443aadfac176657e618c68788ab080648e
brice flaceliere <contact@b-flaceliere.fr>
2017-07-04 20:13:38 +0200

    save

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,1 +1,39 @@
-console.log('Hello World!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+)
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+)
+
+class Hello extends React.Component {
+    render() {
+        return <Router>
+            <div>
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About</Link></li>
+                </ul>
+
+                <hr/>
+
+                <Route exact path="/" component={Home}/>
+                <Route path="/about" component={About}/>
+            </div>
+        </Router>
+    }
+}
+
+ReactDOM.render(<Hello/>, document.getElementById('app'));
